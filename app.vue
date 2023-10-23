5638e4351339b131204a92f3af131ad03772a16d
Kamol <maciek98@poczta.onet.pl>
2023-10-23 18:13:29 +0200

    Edit

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
 <template>
-  <NuxtPage />
+  <div>
+    <VitePwaManifest />
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@vite-pwa/nuxt"],
+  devtools: { enabled: true },
+  pwa: {
+    manifest: {
+      name: "Invoices",
+      short_name: "Invoicesd",
+      description: "Invoices for YOU!",
+      icons: [
+        {
+          src: "icons/icon.jpg",
+          sizes: "500x500",
+          type: "image/png",
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: "/",
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
+});
